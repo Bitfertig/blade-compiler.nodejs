605610c8b0e72b8419c5d2bf91b79613768baaef
BitfertigAdmin <64635171+BitfertigAdmin@users.noreply.github.com>
2020-11-20 00:31:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-class BladeParser
+class BladeCompiler
 {
     /**
      * @param options
@@ -10,9 +10,10 @@ class BladeParser
     {
         this.html = "";
         this.defaultOptions = {
-            extension: '.blade.html',
             folder: './src/views',
             file: '/index',
+            extension: '.blade.html',
+            encoding: 'utf8',
             extends: true,
             regex: {
                 comments: /\{\{\-\-.*\-\-\}\}/gi,
@@ -21,17 +22,16 @@ class BladeParser
 
                 include: /\@include\(\s*[\'\"]([^\[\]\'\"]*)[\'\"]\s*(?:(?:.*[^\s\)])\s*)*\s*\)/gi,
 
-				extends: /\@extends\((?:[\'\"])(.*)(?:[\'\"])\)/gi,
+                extends: /\@extends\((?:[\'\"])(.*)(?:[\'\"])\)/gi,
 
-				yield: /\@yield\([\'\"]?([^\'\"]*)[\'\"]?\)/gi,
-				stack: /\@stack\(\s*[\'\"](.*)[\'\"]\)/gi,
+                yield: /\@yield\([\'\"]?([^\'\"]*)[\'\"]?\)/gi,
+                stack: /\@stack\(\s*[\'\"](.*)[\'\"]\)/gi,
 
                 push: /\@push\(\s*[\'\"]\s*(.*)[\'\"]\s*\)((?:(?!\@endpush|\@stop).*\s*)*)(?:\@endpush|\@stop)|\@push\([\'\"](.*)[\'\"]\s*\,\s*[\"|\'](.*)[\"|\']\s*\)/gi,
 
                 section: /\@section\([\'\"](.*)[\'\"]\s*\,\s*[\'\"](.*)[\'\"]\)|\@section\(\s*[\'\"](.*)[\'\"]\s*\)((?:(?!\@show|\@stop|\@endsection).*\s*)*)(\@show|\@stop|\@endsection)/gi,
                 showSection: /\@section\(\s*[\'\"](.*)[\'\"]\s*\)((?:(?!\@show).*\s*)*)(?:\@show)/gi,
-            },
-            encoding: 'utf8'
+            }
         };
         this.options = Object.assign(this.defaultOptions, options ? options : {});
 
@@ -62,7 +62,7 @@ class BladeParser
     _compile(content)
     {
         let sections = {},
-			stacks = {};
+            stacks = {};
 
         // remove comments
         content = content.replace(this.options.regex.comments, match => "");
@@ -107,26 +107,26 @@ class BladeParser
                 value = secondValue != undefined ? secondValue : firstValue;
 
             if (stacks[key] == undefined) {
-			    stacks[key] = [];
+                stacks[key] = [];
             }
 
-			stacks[key].push(value);
+            stacks[key].push(value);
 
-			return "";
-		});
+            return "";
+        });
 
         // @stack directive
         content = content.replace(this.options.regex.stack, (match, key) => {
-			if (stacks[key] != undefined) {
-				let html = "";
+            if (stacks[key] != undefined) {
+                let html = "";
 
-				stacks[key].forEach(item => html += item);
+                stacks[key].forEach(item => html += item);
 
-				return html;
-			}
+                return html;
+            }
 
-			return "";
-		});
+            return "";
+        });
 
         // @yield directive
         content = content.replace(this.options.regex.yield, (match, key) => {
@@ -169,4 +169,4 @@ class BladeParser
     }
 }
 
-module.exports = options => new BladeParser(options).getHTML();
+module.exports = options => new BladeCompiler(options).getHTML();
